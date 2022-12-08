6a59b26e7e7d6a3d8eb26364e8ed9b0fa4c4dd85
turan.erdogan <turan.erdogan@305-000>
2022-12-08 20:13:40 +0300

    Personel Ozlük Departman Id View'e eklendi

--- pdks/Controllers/OzlukController.cs
+++ pdks/Controllers/OzlukController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using pdks.Models;
 
 namespace pdks.Controllers
 {
     public class OzlukController : Controller
     {
+        PDKSEntities db = new PDKSEntities();
         // GET: Ozluk
         public ActionResult Index()
         {
+
+            var Veri = db.PersonelOzlukBilgileri.ToList();
+
+
+            return View(Veri);
+        }
+        [HttpGet]
+        public ActionResult Giris()
+
+        {
+
             return View();
+        }
+        [HttpPost]
+        public ActionResult Giris(Models.PersonelOzlukBilgileri Ozluk)
+        {
+
+            db.PersonelOzlukBilgileri.Add(Ozluk);   
+            db.SaveChanges();
+
+            return View();
+;
+
         }
     }
 }

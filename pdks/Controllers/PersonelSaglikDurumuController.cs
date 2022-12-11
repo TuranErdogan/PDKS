ccae964c6fc555397c56ac687691d804c85dae64
bernacaliskann <caliskannberna@gmail.com>
2022-12-11 20:45:49 +0300

    Kayıt Oluşturuldu

--- pdks/Controllers/PersonelSaglikDurumuController.cs
+++ pdks/Controllers/PersonelSaglikDurumuController.cs
@@ -8,12 +8,25 @@ using System.Web.Mvc;
 namespace pdks.Controllers
 {
     public class PersonelSaglikDurumuController : Controller
-    { PDKSEntities db = new PDKSEntities(); 
+    {
+        PDKSEntities db = new PDKSEntities();
         // GET: PersonelSaglikDurumu
         public ActionResult Index()
         {
             var Bilgi = db.PersonelSaglikDurumlari.ToList();
             return View(Bilgi);
+        }
+        public ActionResult KayitEkle()
+        {           
+            return View();
+        }
+        public ActionResult KayitEkle(Models.PersonelSaglikDurumlari Saglik)
+
+        {
+            db.PersonelSaglikDurumlari.Add(Saglik);
+            db.SaveChanges();
+
+            return View();
         }
     }
 }

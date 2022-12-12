e935f61c3b6af6a57d84ee5215c327014746dbdb
mithatvanli <mithatvanli60@gmail.com>
2022-12-12 19:30:01 +0300

    ABCDE-IZINGUNCELLE

--- pdks/Controllers/IzinTakipController.cs
+++ pdks/Controllers/IzinTakipController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -24,13 +25,31 @@ namespace pdks.Controllers
         [HttpPost]
         public ActionResult IzinKayit(Models.IzinTakip ızinTakip)
         {
-
             db.IzinTakip.Add(ızinTakip);
             db.SaveChanges();
+            return View();  /*aaaaaaaaaaa*/           
 
-            return View();  /*aaaaaaaaaaa*/
-            
+        }
+        [HttpGet]
+        public ActionResult Update1(int Id) 
+        {
+            if (Id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Models.IzinTakip PersonelIzın = db.IzinTakip.Find(Id);
 
+            return View(PersonelIzın);
+        }
+        public ActionResult Update1(Models.IzinTakip Ekran) 
+        {
+            if (ModelState.IsValid)// doldurulması zorunlu alanlar doldurulmuş ise
+            {
+                db.Entry(Ekran).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");   
+            }
+            return View(Ekran);
         }
     }
 }

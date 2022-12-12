e413bc0bf7cc3d6dd3997eac4362bbe1b407a345
humeyraalemdar <113635918+humeyraalemdar@users.noreply.github.com>
2022-12-13 00:12:03 +0300

    Puantaj Son Düzenleme

--- pdks/Controllers/PuantajController.cs
+++ pdks/Controllers/PuantajController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using pdks.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +10,60 @@ namespace pdks.Controllers
 {
     public class PuantajController : Controller
     {
+        PDKSEntities db = new PDKSEntities();
         // Paylaş
         // GET: Puantaj
         public ActionResult Index()
         {
+            var Veri = db.PersonelOzlukBilgileri.ToList();
+
+
+            return View(Veri);
+        }
+        [HttpGet]
+        public ActionResult Ekle()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ekle(Models.PersonelPuantaj Puantaj)
+        {
+
+            db.PersonelPuantaj.Add(Puantaj);
+            db.SaveChanges();
+
+            return View();
+            ;
+
+        }
+
+        [HttpGet]
+        public ActionResult Guncelle(int id)
+        {
+
+            if (id == null)
+            {
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Models.PersonelPuantaj PersonelP = db.PersonelPuantaj.Find(id);
+
+
+            return View(PersonelP);
+        }
+        [HttpPost]
+        public ActionResult Guncelle(Models.PersonelPuantaj Ekran)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(Ekran).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(Ekran);
+
         }
     }
 }

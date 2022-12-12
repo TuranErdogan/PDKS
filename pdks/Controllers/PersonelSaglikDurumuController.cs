a3842dd3a070f561b57f8db51a5afd84b3308e45
bernacaliskann <caliskannberna@gmail.com>
2022-12-12 19:28:04 +0300

    güncelleme yapıldı

--- pdks/Controllers/PersonelSaglikDurumuController.cs
+++ pdks/Controllers/PersonelSaglikDurumuController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -29,6 +30,35 @@ namespace pdks.Controllers
             db.SaveChanges();
 
             return View();
+        }
+        [HttpGet]
+        public ActionResult Güncelle(int id )
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                
+            }
+            Models.PersonelSaglikDurumlari PersonelSag = db.PersonelSaglikDurumlari.Find(id);
+            //modelim var gıt db den tabloyu ara ıd ye gore bul modelin içine bırak.personelsag dosyası olusur.
+            return View(PersonelSag);
+        }
+
+        [HttpPost]
+        public ActionResult Güncelle (Models.PersonelSaglikDurumlari Saglik)
+        {
+            //veritabanıındaki boşluk sartlarına uygunsa verilen veri veritabanın ozelliklerine uyuyorsa
+            if (ModelState.IsValid)
+            {
+                //state yapısal ozellık demek.
+                //saglık ekranını al sistemde guncelle
+                db.Entry(Saglik).State = System.Data.Entity.EntityState.Modified;
+                //kaydet
+                db.SaveChanges();
+                //lıstele. (index yazmamın sebebi indexte listelediğim için)
+                return RedirectToAction("Index");
+            }
+            return View(Saglik);
         }
     }
 }
